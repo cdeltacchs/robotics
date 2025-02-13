6c56ef52f044455f2d14c04346c7d6561e2f938e
thepollitosd <diegobarriosb10@icloud.com>
2025-02-12 16:28:27 -0800

    Updated Constants.java
    Added all motor controllers and made sure all libraries are updated and added.

--- dbarba/5025RobotDDrivetrain/src/main/java/frc/robot/Constants.java
+++ dbarba/5025RobotDDrivetrain/src/main/java/frc/robot/Constants.java
@@ -4,20 +4,55 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+/* 
+   *
+   * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+   * constants. This class should not be used for any other purpose. All constants should be declared
+   * globally (i.e. public static). Do not put anything functional in this class.
+   *
+   * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+   * constants are needed, to reduce verbosity.
  */
 public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
 
-    public static final Joystick joystickOne = new Joystick(0);
+    // Controls the ports for both of the Joysticks
+    public static final int kJoystickOnePort = 0;
+    public static final int kJoystickTwoPort = 1;
+
+    
+    public static final int kTTBEncoderChannelA = 0;
+    public static final int kTTBEncoderChannelB = 1;
+
+    public static final int kLFMCPort = 21; // Left Front Motor Controller
+    public static final int kLRMCPort = 22; // Left Rear Motor Controller
+    public static final int kRFMCPort = 23; // Right Front Motor Controller
+    public static final int kRRMCPort = 24; // Right Rear Motor Controller
+
+    public static final int kLWMCPort = 96; // Right Front Motor Controller
+    public static final int kRWMCPort = 97; // Right Rear Motor Controller
+
+    // Init encoders
+    public static final Encoder ttbEncoder = new Encoder(kTTBEncoderChannelA, kTTBEncoderChannelB);
+
+    // Init joystick controllers
+    public static final Joystick joystickOne = new Joystick(kJoystickOnePort);
+    public static final Joystick joystickTwo = new Joystick(kJoystickTwoPort);
+
+    // Init drivetrain motor controllers
+    public static final TalonSRX leftFrontMotor = new TalonSRX(kLFMCPort);
+    public static final TalonSRX leftRearMotor = new TalonSRX(kLRMCPort);
+    public static final TalonSRX rightFrontMotor = new TalonSRX(kRFMCPort);
+    public static final TalonSRX rightRearMotor = new TalonSRX(kRRMCPort);
+
+    // Init wench motor controllers
+    public static final TalonSRX leftWenchMotor = new TalonSRX(kLWMCPort);
+    public static final TalonSRX rightWenchMotor = new TalonSRX(kRWMCPort);
   }
 }
