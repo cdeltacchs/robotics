7239e34b23672736508dc6ef31a0fed40d19650b
thepollitosd <diegobarriosb10@icloud.com>
2025-02-13 16:29:06 -0800

    Update DriveSubsystem.java

--- dbarba/5025RobotDDrivetrain/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ dbarba/5025RobotDDrivetrain/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,6 +4,8 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.OperatorConstants;
@@ -29,10 +31,30 @@ public class DriveSubsystem extends SubsystemBase {
 
   @Override
   public void periodic() {
+    //Smart Dashboard value display
     SmartDashboard.putNumber("Encoder Value: ", OperatorConstants.ttbEncoder.getDistance());
     SmartDashboard.putNumber("Joystick 1 X Axis: ", OperatorConstants.joystickOne.getRawAxis(0));
     SmartDashboard.putNumber("Joystick 1 Y Axis: ", OperatorConstants.joystickOne.getRawAxis(1));
     SmartDashboard.putNumber("Joystick 1 Z Axis: ", OperatorConstants.joystickOne.getRawAxis(2));
     SmartDashboard.putString("Does it work? ", "Yes");
+  }
+
+  public void set(double speed, double turn) {
+    OperatorConstants.leftRearMotor.follow(OperatorConstants.leftFrontMotor);// Fix
+    OperatorConstants.leftFrontMotor.set(ControlMode.PercentOutput, turn);// Fix but close
+    OperatorConstants.rightRearMotor.follow(OperatorConstants.rightFrontMotor);// Fix
+    OperatorConstants.rightFrontMotor.set(ControlMode.PercentOutput, turn);// Fix but close
+    SmartDashboard.putNumber("Left Speed", OperatorConstants.leftFrontMotor.getActiveTrajectoryVelocity());// Fix
+    SmartDashboard.putNumber("Right Speed", OperatorConstants.rightFrontMotor.getActiveTrajectoryVelocity());// Fix
+  }
+
+  public void tankMode(double left, double right) {}
+
+  public void stop() {
+    // Neutralizes motor output
+    OperatorConstants.leftFrontMotor.neutralOutput();
+    OperatorConstants.leftRearMotor.neutralOutput();
+    OperatorConstants.rightFrontMotor.neutralOutput();
+    OperatorConstants.rightRearMotor.neutralOutput();
   }
 }
